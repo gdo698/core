04dfbd4433bb1ebf2ead40823ae7bba241d6d81b
parad32 <rlarjsdn0327@gmail.com>
2025-04-23 18:14:13 +0900

    first commit

---
+++ src/main/java/com/core/ErpPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpPosSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpPosSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/core/erp/ErpPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpPosSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
