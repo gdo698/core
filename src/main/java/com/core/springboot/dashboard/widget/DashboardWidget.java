7b0e04a69390eb045b9de69fce2d7221af3259b6
para32 <rlarjsdn0327@gmail.com>
2025-05-12 23:31:26 +0900

    <기능><수정>컴파일에러 오류해결결

--- src/main/java/com/core/springboot/dashboard/widget/DashboardWidget.java
+++
@@ -1,8 +0,0 @@
-package com.core.springboot.dashboard.widget;
-
-import com.core.springboot.dashboard.model.WidgetData;
-
-public interface DashboardWidget {
-    String getWidgetCode();
-    WidgetData getWidgetData(Long storeId);
-} 
