259faa2748c0ff96d1b83c780dceb1674130619f
dyfrkfl1 <dyfrkfl1@gmail.com>
2025-05-16 09:40:43 +0900

    <수정> stock_transfer 테이블 생성으로 인한 drop문 수정

--- src/main/resources/sql/drop.sql
+++ src/main/resources/sql/drop.sql
@@ -33,6 +33,7 @@ DROP TABLE IF EXISTS `stock_inventory_check_item`;
 DROP TABLE IF EXISTS `stock_inventory_check`;
 DROP TABLE IF EXISTS `hq_stock`;
 DROP TABLE IF EXISTS `stock_flow`;
+DROP TABLE IF EXISTS `stock_transfer`;
 DROP TABLE IF EXISTS `product`;
 DROP TABLE IF EXISTS `weather_data`;
 DROP TABLE IF EXISTS `ai_model`;
